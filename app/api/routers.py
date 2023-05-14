f608e990dc518c07c5bab9df5adc26b8af46b64a
Илья Соболев <i4sobolev@yandex.ru>
2023-05-14 18:32:22 +0200

    before 1 review

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import (charity_project_router, donation_router,
-                               user_router)
+from app.api.endpoints import (charity_project_router,
+                               donation_router,
+                               user_router,
+                               google_api_router)
 
 
 main_router = APIRouter()
@@ -12,3 +14,6 @@ main_router.include_router(
     donation_router, prefix='/donation', tags=['Donations']
 )
 main_router.include_router(user_router)
+main_router.include_router(
+    google_api_router, prefix='/google', tags=['Google']
+)
--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
@@ -32,6 +32,20 @@ class CRUDCharityProject(CRUDBase):
         )
         db_project = db_project.scalars().first()
         return db_project
+
+    async def get_projects_by_completion_rate(
+        self,
+        session: AsyncSession()
+    ):
+        projects = await session.execute(
+            select(CharityProject).where(
+                CharityProject.fully_invested).order_by(
+                func.julianday(CharityProject.close_date) -
+                func.julianday(CharityProject.create_date)
+            )
+        )
+        projects = projects.scalars().all()
+        return projects
 
 
 charity_project_crud = CRUDCharityProject(CharityProject)
